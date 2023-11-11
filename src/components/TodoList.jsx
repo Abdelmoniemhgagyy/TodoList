068255a0f14c5d5c556525771aacbd5a63b8240e
Abdelmoniemhgagyy <moniemhgagy351@gmail.com>
2023-11-11 21:09:34 +0200

    update style

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { useState } from 'react'
+import { createRef } from 'react';
+import { useState,useEffect } from 'react'
 
 function TodoList() {
     const[item,setItem]=useState("")
     const[todos,setTodos]=useState([]);
+    
     const idhandel = ()=> {
       return  Math.floor(Math.random()*10)
     }
+    const input = createRef();
+    useEffect(()=>{
+      input.current.focus();
+    },[])
     const handeltodos = ()=>{
         
         if(item !== ""){
@@ -22,7 +28,9 @@ function TodoList() {
 //         id:idhandel(),
 //        }) 
 //    )
-       setItem("")
+       setItem("");
+       input.current.focus();
+
     }
     const remove = (id)=> {
          setTodos(todos.filter((item)=> item.id !==id ))
@@ -30,19 +38,21 @@ function TodoList() {
     const clearAll = ()=> {
          setTodos([])
     }
+
   return (
     <div className='todo-container'>
-        <input type="text" value={item} onChange={(e)=> setItem(e.target.value)}/>
+        <input type="text" value={item} onChange={(e)=> setItem(e.target.value)} ref={input}/>
         <button onClick={()=>handeltodos(item)}>Add item</button>
 
         <ul>
-        {todos.length>0 && todos.map(({text,id})=>
-        <><li key={id}>{text}
-         <button onClick={()=> remove(id)}>x</button>
+        {todos.length>0 && todos.map((todo)=>
+        <><li key={todo.id}>{todo.text}
+         <button onClick={()=> remove(todo.id)}>x</button>
+        
         </li>
         </>
         )}
-       {todos.length>0 &&<button className='clearAll' onClick={()=> clearAll()}><i className='bi bi-trash3-fill'></i></button>} 
+       {todos.length>0 &&<button className='clearAll' ><i onClick={()=> clearAll()} className='bi bi-trash3-fill'></i></button>} 
         </ul>
          
     </div>
